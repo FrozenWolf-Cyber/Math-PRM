49c6fee369473ad3d7537dd7d0f54978d5354976
Gokul Adethya <frozenwolf2003.com@gmail.com>
2025-07-15 23:51:11 +0530

    memory debug

--- prm_data.py
+++ prm_data.py
@@ -218,6 +218,7 @@ class QwenMathMetaDataset(Dataset):
         self.len = 0
 
         df = self.dataset.to_pandas()
+        print(f"Dataset loaded with {len(df)} samples.")
         if filter_dataset_steps > 0:
             data = df
             print(f"Filtering dataset to steps <= {filter_dataset_steps}")
@@ -225,7 +226,7 @@ class QwenMathMetaDataset(Dataset):
             df = data
             self.dataset = HF_Dataset.from_pandas(data)
         
-
+        print(f"Total number of samples in the dataset: {len(df)}")
         # Count total words in the completions list
         df['total_word_count'] = df['completions'].progress_apply(lambda steps: sum(len(tokenizer(step)['input_ids']) for step in steps))
 
