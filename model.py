63f5a97c17cdd0f27a7858cad906b2f2becc28fd
Gokul Adethya <frozenwolf2003.com@gmail.com>
2025-07-16 13:46:32 +0530

    memory debug

--- model.py
+++ model.py
@@ -69,10 +69,8 @@ class QwenMathTokenClf_RM(nn.Module):
         logits = outputs.logits
         logits = self.LN(logits).to(dtype=torch.float)  # Apply linear layer to logits
         # print(outputs)
-        print("Logits shape:", logits)
-        logits = F.softmax(logits)[..., 1]  # Assuming the second class is the one we want to predict
+        logits = F.softmax(logits, dim=-1)[..., 1]  # Assuming the second class is the one we want to predict
         # print(value_outputs)
-        print("Processed logits shape:", logits)
         return logits.squeeze(dim=1)
         
         
