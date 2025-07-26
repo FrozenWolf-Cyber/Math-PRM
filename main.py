1cb5001f0ce90316388a5d9a17452f82d9162a8a
Gokul Adethya <frozenwolf2003.com@gmail.com>
2025-07-26 09:51:26 +0530

    fix lora loading

--- main.py
+++ main.py
@@ -409,7 +409,7 @@ class Lower(ImplicitProblem):
         
         if args.load_path != "":
             if args.peft_rank != -1:
-                model.base_model = PeftModel.from_pretrained(model, f"{args.load_path}/lower_weights")
+                model.base_model = PeftModel.from_pretrained(model.base_model, f"{args.load_path}/lower_weights")
             else:
                 model.base_model.from_pretrained(f"{args.load_path}/lower_weights")
             model.LN.load_state_dict(torch.load(f"{args.load_path}/lower_weights_LN.pt"))
